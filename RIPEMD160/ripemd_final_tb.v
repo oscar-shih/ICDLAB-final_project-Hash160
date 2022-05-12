906acce69969d998e4cfa9273a3a05b145fc21cd
Brian0003 <83951882+Boentsai@users.noreply.github.com>
2022-05-13 00:26:23 +0800

    Update ripemd_final_tb.v

--- RIPEMD160/ripemd_final_tb.v
+++ RIPEMD160/ripemd_final_tb.v
@@ -5,8 +5,11 @@
 module ripemd160_tb;
 
 	//pins
+    integer idx=0;
+    reg [511:0] inputt_r[0:99];
     reg [511:0] inputt;
     wire [159:0] ans;
+    reg [159:0]test[0:99];
 
     reg clk, rst_n, next, init;
     wire ready, done;
@@ -28,39 +31,34 @@ module ripemd160_tb;
     `endif   
 
     always #10 clk = ~clk;
-
-
 	//initial block
 	initial begin
         $fsdbDumpfile("ripemd160.fsdb");
 	    $fsdbDumpvars(0, "+mda");
+        $readmemh("ripemd_input.txt",inputt_r);
+        $readmemh("ripemd_output.txt",test);
+        $display("starting");
 		$display("Testbench Start.");
+        repeat(100)  begin 
         rst_n = 1;
         clk = 0;
         init = 0;
         next = 0;
-        inputt = 512'h0000000000000058000000000000000000000000000000000000000000000000000000000000000000000000000000000000000080676e61592072657473614d;
         #10 rst_n = 0;
         #10 rst_n = 1;
 		init = 1;
+        inputt=inputt_r[idx];
 		@(posedge done);
-			
-			//check answer
-        // if(ans != 160'h3b7b334f_d5da9f29_b0f102a1_7c1e16db_ee997931) $display("error!!! out =  %h;", ans);
-        // else $display("Success!!");
-        // if(ans !== 160'h4dc9d8f5_7a10181a_0c78d60c_a2e95122_cc4fbacc) $display("error!!! out =  %h;", ans);
-        // else $display("Success!!");
-        if(ans !== 160'hfedcd200dc0f8b4c51ad7ae734282ff2008bbb12) $display("error!!! out =  %h;", ans);
-        else $display("Success!!");
-			//end
-        #50;
-		
+        if(ans !== test[idx]) $display("error!!!%d out =  %h;",idx, ans);
+        else $display("Success%d %h!!",idx, ans);
+        idx=idx+1;
+        end
 		$display("Testbench End.");
 		$finish;
 	end
 
-    initial begin
-        # 10000 $display("Time Exceed.") ;
-        $finish;
-    end
+    // initial begin
+    //     # 10000 $display("Time Exceed.") ;
+    //     $finish;
+    // end
 endmodule
