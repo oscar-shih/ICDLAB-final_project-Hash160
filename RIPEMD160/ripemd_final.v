ee5f1adf9d1f74001d1277d1649757ca12e4e095
jimmyb08901017 <56395377+jimmyb08901017@users.noreply.github.com>
2022-05-12 17:53:23 +0800

    v1 update

---
+++ RIPEMD160/ripemd_final.v
@@ -0,0 +1,75 @@
+`include "ripemd160.v"
+`include "ripemd160_2.v"
+
+module ripemd_final(
+                   input wire            clk,
+                   input wire            rst_n,
+                   input wire            i_valid,
+                   input wire [511 : 0]  block,
+
+                   output wire           o_valid,
+                   output wire [159 : 0] ans
+                  );
+
+wire [31:0] a1, b1, c1, d1, e1;
+wire [31:0] a2, b2, c2, d2, e2;
+wire [31:0] h0_w, h1_w, h2_w, h3_w, h4_w;
+reg [31:0] h0_r, h1_r, h2_r, h3_r, h4_r;
+
+reg done, done_r;
+
+assign o_valid = done_r;
+
+RIPEMD160_stage_1_core ripemd160_right(
+    .clk_p_i(clk),
+    .rst_n(rst_n),
+    .i_valid(i_valid),
+    .block({block}),
+    .o_valid(done),
+    .ans({a1,b1,c1,d1,e1})
+);
+
+RIPEMD160_stage_2_core ripemd160_left(
+    .clk(clk),
+    .rst_n(rst_n),
+    .i_valid(i_valid),
+    .block({block}),
+    .o_valid(), //done
+    .ans({a2,b2,c2,d2,e2})
+);
+
+///////// Answer Calculation ////////
+localparam RIPEMD160_H0 = 32'h67452301;
+localparam RIPEMD160_H1 = 32'hefcdab89;
+localparam RIPEMD160_H2 = 32'h98badcfe;
+localparam RIPEMD160_H3 = 32'h10325476;
+localparam RIPEMD160_H4 = 32'hc3d2e1f0;
+
+assign h0_w = RIPEMD160_H1 + c1 + d2;
+assign h1_w = RIPEMD160_H2 + d1 + e2;
+assign h2_w = RIPEMD160_H3 + e1 + a2;
+assign h3_w = RIPEMD160_H4 + a1 + b2;
+assign h4_w = RIPEMD160_H0 + b1 + c2;
+assign o_answer = {h0_r, h1_r, h2_r, h3_r, h4_r};
+
+
+
+always @(posedge clk or negedge rst_n) begin
+    if(!rst_n) begin
+        h0_r <= 32'b0;
+        h1_r <= 32'b0;
+        h2_r <= 32'b0; 
+        h3_r <= 32'b0; 
+        h4_r <= 32'b0;
+        done_r <= 0;
+    end
+    else begin
+        h0_r <= h0_w;
+        h1_r <= h1_w;
+        h2_r <= h2_w; 
+        h3_r <= h3_w; 
+        h4_r <= h4_w;
+        done_r <= done;
+    end
+end
+endmodule
---
+++ RIPEMD160/ripemd_final_tb.v
@@ -0,0 +1,70 @@
+`timescale 1ns/10ps
+`define CYCLE    10          	         // Modify your clock period here
+`define SDFFILE    "./stage_2_test.sdf"	      // Modify your sdf file name
+
+module ripemd_tb;
+
+	//pins
+    reg [511:0] inputt;
+    wire [159:0] ans;
+
+    reg clk, rst_n, next, init;
+    wire ready, done;
+    // wire [159:0] ans;
+	//module instantiation
+	ripemd_final r0(
+                   .clk(clk),
+                   .rst_n(rst_n),
+                   .i_valid(init),
+                   // .next(next),
+                   .block(inputt),
+                   // .ready(ready),
+                   .ans(ans),
+                   .o_valid(done)
+                  );
+					  
+    `ifdef SDF
+        initial $sdf_annotate(`SDFFILE, r0);
+    `endif   
+
+    always #10 clk = ~clk;
+
+
+	//initial block
+	initial begin
+		$display("Testbench Start.");
+        rst_n = 1;
+        clk = 0;
+        init = 0;
+        next = 0;
+        // inputt = 512'h7473614d_59207265_80676e61_00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000;
+        inputt = 512'h00000000_00000058_00000000000000000000000000000000000000000000000000000000000000000000000000000000_00000000_80676e61_59207265_7473614d;
+        // inputt = 0;
+        //inputt = 512'h80393837_36353433_3231307a_79787776_75747372_71706f6e_6d6c6b6a_69686766_65646362_615a5958_57565554_53525150_4f4e4d4c_4b4a4948_47464544_43424122;
+        #10 rst_n = 0;
+        #10 rst_n = 1;
+        // @(posedge ready);
+		// #50;
+
+        
+        // inputt = 512'h0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000080676e61592072657473614d;
+		init = 1;
+		@(posedge done);
+			
+			//check answer
+        // if(ans != 160'h3b7b334f_d5da9f29_b0f102a1_7c1e16db_ee997931) $display("error!!! out =  %h;", ans);
+        // else $display("Success!!");
+        if(ans != 160'h4dc9d8f5_7a10181a_0c78d60c_a2e95122_cc4fbacc) $display("error!!! out =  %h;", ans);
+        else $display("Success!!");
+			//end
+        #50;
+		
+		$display("Testbench End.");
+		$finish;
+	end
+
+    initial begin
+        # 10000 $display("Time Exceed.") ;
+        $finish;
+    end
+endmodule
